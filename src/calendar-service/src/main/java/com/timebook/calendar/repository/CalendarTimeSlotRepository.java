149653bbb21748f88400c8e175ea95db4306d765
Denis Balan <DenisBalan@users.noreply.github.com>
2021-03-01 14:03:19 +0200

    Add dockerfile for all projects

--- src/calendar-service/src/main/java/com/timebook/calendar/repository/CalendarTimeSlotRepository.java
+++
@@ -1,8 +0,0 @@
-package com.timebook.calendar.repository;
-
-import com.timebook.calendar.domain.CalendarTimeSlot;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CalendarTimeSlotRepository extends MongoRepository<CalendarTimeSlot,Long> {
-    
-}
