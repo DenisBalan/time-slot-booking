741d7e9a4dbe150c53cbc1e2226e485c907f87f6
Denis Balan <DenisBalan@users.noreply.github.com>
2021-03-15 12:40:26 +0200

    Add time-slot-notification-service-mongodb container

--- src/calendar-service/src/main/java/com/timebook/calendar/domain/timeseries/RangeTimeSlot.java
+++ src/calendar-service/src/main/java/com/timebook/calendar/domain/timeseries/RangeTimeSlot.java
@@ -1,21 +1,11 @@
 package com.timebook.calendar.domain.timeseries;
 
-import com.timebook.calendar.domain.Currency;
-import com.timebook.calendar.domain.TimePeriod;
 
-import java.math.BigDecimal;
 import java.util.*;
 
-@Getter
-@Setter
 public class RangeTimeSlot {
 
-	private Date begin;
+	public Date begin;
 
-	private Date end;
-
-	public RangeTimeSlot(Date begin, Date end) {
-		this.begin = begin;
-		this.end = end;
-	}
+	public Date end;
 }
